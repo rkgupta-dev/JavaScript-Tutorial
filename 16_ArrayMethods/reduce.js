ac46e6901d62650353a15270b9dccfe5f633c0b9
rkgupta-dev <rohitkrgupta333@gmail.com>
2024-10-30 12:48:50 +0530

    updates reduce()

--- 16_ArrayMethods/reduce.js
+++ 16_ArrayMethods/reduce.js
@@ -17,6 +17,47 @@ console.log("Total= " + total);
 
 let numbers = [5, 10, 3, 7, 8];
 
-let max = numbers.reduce((acc, current) => (current > acc ? current : acc), numbers[0]);
+let max = numbers.reduce(
+  (acc, current) => (current > acc ? current : acc),
+  numbers[0]
+);
 console.log("Maxmium= " + max);
 
+/*
+3. Counting Occurrences of Items in an Array
+-> Using reduce() to count the occurrences of each item in an array.   */
+
+let fruits = ["apple", "banana", "apple", "orange", "banana", "apple"];
+
+let count = fruits.reduce((acc, fruit) => {
+  acc[fruit] = (acc[fruit] || 0) + 1;
+  return acc;
+}, {});
+
+console.log(count);
+
+/*
+4. Flattening an Array of Arrays
+-> You can flatten a nested array into a single array using reduce().  */
+
+let nestedArray = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+
+let flatted = nestedArray.reduce((acc, current) => acc.concat(current), []);
+console.log(flatted);
+
+/*
+5. Calculating the Product of Numbers in an Array
+-> Using reduce() to calculate the product of all numbers in an array.   */
+
+let number1 = [2, 3, 4];
+
+let product = number1.reduce((acc, current) => acc * current, 1);
+console.log(product);
+
+/*
+6. Grouping Items by Property in an Array of Objects
+-> Using reduce() to group an array of objects by a specific property.   */
